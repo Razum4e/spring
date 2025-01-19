b7be8ee1b08f5276e178eb5dfc19814b8b686c23
arazumov <Razum.ya@ya.ru>
2025-01-19 17:17:31 +0300

    added edit operation tasks by id

--- src/main/java/com/github/razum4e/spring/TaskController.java
+++ src/main/java/com/github/razum4e/spring/TaskController.java
@@ -1,12 +1,20 @@
 package com.github.razum4e.spring;
 
+import com.github.razum4e.spring.model.Task;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-import com.github.razum4e.spring.model.Task;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 public class TaskController {
+
+    private static final Sort DEFAULT_SORT_ORDER = Sort.by(Sort.Direction.DESC, "id");
 
     private final TaskRepository taskRepository;
 
@@ -16,7 +24,8 @@ public class TaskController {
 
     @GetMapping("/")
     public String home(Model model) {
-        model.addAttribute("tasks", taskRepository.findAll());
+        List<Task> tasks = taskRepository.findAll(DEFAULT_SORT_ORDER);
+        model.addAttribute("tasks", tasks);
         return "index";
     }
 
@@ -29,8 +38,19 @@ public class TaskController {
         return "redirect:/";
     }
 
+    @PostMapping("/edit/{id}")
+    public String editTask(@PathVariable long id, @RequestParam String name, @RequestParam String description) {
+        taskRepository.findById(id)
+                .ifPresent(task -> {
+                    task.setName(name);
+                    task.setDescription(description);
+                    taskRepository.save(task);
+                });
+        return "redirect:/";
+    }
+
     @PostMapping("/delete/{id}")
-    public String deleteTask(@PathVariable Long id) {
+    public String deleteTask(@PathVariable long id) {
         taskRepository.deleteById(id);
         return "redirect:/";
     }
--- src/main/java/com/github/razum4e/spring/TaskRepository.java
+++ src/main/java/com/github/razum4e/spring/TaskRepository.java
@@ -1,7 +1,7 @@
 package com.github.razum4e.spring;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import com.github.razum4e.spring.model.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TaskRepository extends JpaRepository<Task, Long> {
 }

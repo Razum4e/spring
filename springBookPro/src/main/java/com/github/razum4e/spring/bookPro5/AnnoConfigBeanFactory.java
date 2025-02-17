9b36e7b115740ec2ac4a1e1b879491fc9c3e9d3c
arazumov <Razum.ya@ya.ru>
2025-02-17 21:27:07 +0300

    refactoring

--- springBookPro/src/main/java/com/github/razum4e/spring/bookPro5/AnnoConfigBeanFactory.java
+++ springBookPro/src/main/java/com/github/razum4e/spring/bookPro5/AnnoConfigBeanFactory.java
@@ -1,14 +1,32 @@
 package com.github.razum4e.spring.bookPro5;
 
-import com.github.razum4e.spring.bookPro5.message.HelloWorldConfiguration;
-import com.github.razum4e.spring.bookPro5.message.MessageRenderer;
+import com.github.razum4e.spring.bookPro5.message.*;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 public class AnnoConfigBeanFactory {
     public static void main(String[] args) {
         ApplicationContext ctx = new AnnotationConfigApplicationContext(HelloWorldConfiguration.class);
         MessageRenderer mr = ctx.getBean("renderer", MessageRenderer.class);
         mr.render();
+    }
+
+    //@ComponentScan(basePackages = "com.github.razum4e.spring.bookPro5.message")
+    //тогда не нужны @Bean's и методы
+    @Configuration
+    public static class HelloWorldConfiguration {
+        @Bean
+        public MessageProvider provider() {
+            return new HelloWorldMessageProvider();
+        }
+
+        @Bean
+        public MessageRenderer renderer(){
+            MessageRenderer renderer = new StandardOutMessageRenderer();
+            renderer.setMessageProvider(provider());
+            return renderer;
+        }
     }
 }
--- springBookPro/src/main/java/com/github/razum4e/spring/bookPro5/XmlConfigWithBeanFactory.java
+++ springBookPro/src/main/java/com/github/razum4e/spring/bookPro5/XmlConfigWithBeanFactory.java
@@ -3,6 +3,7 @@ package com.github.razum4e.spring.bookPro5;
 import com.github.razum4e.spring.bookPro5.message.MessageProvider;
 import com.github.razum4e.spring.bookPro5.message.MessageRenderer;
 import com.github.razum4e.spring.bookPro5.oracle.Oracle;
+import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
 import org.springframework.context.ApplicationContext;
@@ -10,33 +11,29 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 import org.springframework.core.io.ClassPathResource;
 
-import static com.github.razum4e.spring.bookPro5.XmlConfigWithBeanFactory.Configuration.XML_FILE_MESSAGE;
+import static com.github.razum4e.spring.bookPro5.XmlConfigWithBeanFactory.Configuration.XML_FILE_CONFIG;
 import static com.github.razum4e.spring.bookPro5.XmlConfigWithBeanFactory.Configuration.XML_FILE_MESSAGE_ANNO;
-import static com.github.razum4e.spring.bookPro5.XmlConfigWithBeanFactory.Configuration.XML_FILE_ORACLE;
 
 public class XmlConfigWithBeanFactory {
     interface Configuration {
-        String XML_FILE_MESSAGE = "config_message.xml";
+        String XML_FILE_CONFIG = "config_message.xml";
         String XML_FILE_MESSAGE_ANNO = "config_message_annotation.xml";
-        String XML_FILE_ORACLE = "config_oracle.xml";
     }
 
     public static void main(String... args) {
         ApplicationContext ctx = new ClassPathXmlApplicationContext(
-                XML_FILE_MESSAGE,
+                XML_FILE_CONFIG,
                 XmlConfigWithBeanFactory.class
         );
-        MessageRenderer mr = ctx.getBean("renderer", MessageRenderer.class);
-        mr.render();
+        runGetBean(ctx);
 
         DefaultListableBeanFactory factory = new DefaultListableBeanFactory();
         XmlBeanDefinitionReader rdr = new XmlBeanDefinitionReader(factory);
         rdr.loadBeanDefinitions(new ClassPathResource(
-                XML_FILE_ORACLE,
+                XML_FILE_CONFIG,
                 XmlConfigWithBeanFactory.class
         ));
-        Oracle oracle = (Oracle) factory.getBean("oracle");
-        System.out.println(oracle.defineMeaningOfLife());
+        runGetBean(factory);
 
         GenericXmlApplicationContext genericCtx = new GenericXmlApplicationContext();
         genericCtx.load(
@@ -47,5 +44,12 @@ public class XmlConfigWithBeanFactory {
         MessageProvider messageProvider = genericCtx.getBean("provider", MessageProvider.class);
         System.out.println(messageProvider.getMessage());
         genericCtx.close();
+    }
+
+    private static void runGetBean(BeanFactory ctx) {
+        MessageRenderer mr = ctx.getBean("renderer", MessageRenderer.class);
+        mr.render();
+        Oracle oracle = ctx.getBean("oracle", Oracle.class);
+        System.out.println(oracle.defineMeaningOfLife());
     }
 }
